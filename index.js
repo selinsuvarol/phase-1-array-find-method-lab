3c5357ab2c240bf548c1a333e98b07121e402dfd
Selin Su Varol <selinsuvarol@gmail.com>
2022-04-19 20:26:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const arrayOfObjects = [
+    {year:"2015", result: "W"},
+    {year:"2014", result: "n/a"},
+    {year:"2013", result: "T"},
+]
+
+function superbowlWin(array) {
+    const result = array.find((arr) => arr.result === "W");
+    if (result) {
+        return result.year;
+    }else{
+        return undefined;
+    };
+};
